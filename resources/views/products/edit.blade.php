0b8c8116ca2f093ef79ac7fa08339f4f0471d3c1
Ry Samly <chp051970@gmail.com>
2025-08-09 16:34:59 +0700

    third commit

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -39,6 +39,7 @@
         </div>
 
         <button type="submit" class="btn btn-primary">Update</button>
+        <a href="{{ route('products.index') }}" class="btn btn-secondary">Cancel</a>
     </form>
 </div>
 @endsection
